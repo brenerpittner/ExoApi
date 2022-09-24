eda69dc1637942a910603e304a2d5abec0d54eb0
Brener Pittner <pittner@live.com>
2022-09-24 20:36:26 -0300

    commit inicial

---
+++ ExoApi/Contexts/dbExoAPIContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExoApi.Models;
+
+namespace ExoApi.Contexts
+{
+    public class dbExoAPIContext: DbContext
+    {
+        public dbExoAPIContext() { }
+
+        // estancia de um objeto 'option' e retornar direto pro 'DbContext' (base que faz isso)
+        public dbExoAPIContext(DbContextOptions<dbExoAPIContext> options) : base(options) { }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = MYMAX\\SQLEXPRESS; initial catalog = dbExoApi; User Id = aluno; Password = aluno1234"); //User Id = aluno; Password = aluno1234" //Integrated Security = true
+            }
+
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
---
+++ ExoApi/Models/Projeto.cs
@@ -0,0 +1,12 @@
+﻿namespace ExoApi.Models
+{
+    public class Projeto
+    {
+        // Mesmos atributos do banco de dados 'dbExoApi'
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Status { get; set; }
+        public string? Data_De_Inicio { get; set; }
+        public string? Area { get; set; }
+    }
+}
